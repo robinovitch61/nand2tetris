604447252ab5c5e01e9cdfe5a607a887bef21706
robinovitch61 <leorobinovitch@gmail.com>
2019-10-14 18:35:04 -0400

    finish project 4

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,51 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2
+M=0 // initialise result to 0
+
+@sum
+M=0 // sum = 0
+
+@i
+M=0 // i = 0
+
+(LOOP)
+    // if i = R1, go to END
+    @i
+    D=M
+    @R1
+    D=D-M
+    @PROD
+    D;JEQ
+
+    // add R0 to sum
+    @sum
+    D=M
+    @R0
+    D=D+M
+    @sum
+    M=D
+
+    // increment i
+    @i
+    M=M+1
+
+    // keep looping
+    @LOOP
+    0;JMP
+
+(PROD)
+    // set R2 to product so far
+    @sum
+    D=M
+    @R2
+    M=D
+    @END
+    0;JMP
+
+(END)
+    // loop forever
+    @END
+    0;JMP
